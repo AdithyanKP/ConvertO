e41992fbd1098fb2bd6f4c926b3aa0bf0e8ccc57
Adithyan k P <adithyankp777@gmail.com>
2022-05-23 11:22:15 +0530

    basic design

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import styles from "./Home.module.css";
+import { Button } from "@mui/material";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Upload File Here</h1>
+        <form
+          style={{ padding: 20, display: "flex", flexDirection: "row" }}
+          action="upload"
+          method="post"
+          enctype="multipart/form-data"
+        >
+          <input type="file" name="upfile" value="" />
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Button variant="outlined" size="small">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Home;
